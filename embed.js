48a0606da49323aaf165ec75ea60409ce2517404
JD Bothma <jbothma@gmail.com>
2016-03-24 17:37:55 +0200

    Remove all traces of storybuilder

--- embed.js
+++ embed.js
@@ -4,5 +4,5 @@ if (document.location.hostname == "localhost") {
     var baseurl = "https://static.code4sa.org/forgotten-people/";
 }
 document.write('<div id="code4sa-forgotten-people-index"></div>');
-document.write('<script type="text/javascript" src="' + baseurl + 'js/pym.min.js"></script>');
+document.write('<script type="text/javascript" src="' + baseurl + 'js/pym.js"></script>');
 document.write("<script>var pymParent = new pym.Parent('code4sa-forgotten-people-index', '" + baseurl + "index.html', {});</script>");
